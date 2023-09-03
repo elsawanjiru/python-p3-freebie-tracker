88b67be51b26367caf3e69f48e2197eb45902e19
elsawanjiru <elsa.wanjiru@student.moringaschool.com>
2023-09-03 19:16:41 +0300

    Completed code challenge

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,15 @@
-#!/usr/bin/env python3
+from alembic import op
+import sqlalchemy as sa
 
-# Script goes here!
+def upgrade():
+    op.create_table('freebies',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('item_name', sa.String(), nullable=False),
+        sa.Column('value', sa.Integer(), nullable=False),
+        sa.Column('dev_id', sa.Integer(), sa.ForeignKey('dev.id'), nullable=False),
+        sa.Column('company_id', sa.Integer(), sa.ForeignKey('company.id'), nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+def downgrade():
+    op.drop_table('freebies')
